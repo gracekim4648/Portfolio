b2ec798169b3863416f9e5b766efd1aaac0de123
gdking137 <gdkim137@gmail.com>
2023-03-23 17:09:56 -0400

    JS: project filtering

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
-  console.log(1 - window.scrollY / homeHeight);
+  // console.log(1 - window.scrollY / homeHeight);
 });
 
 // Show arrow-up button when scrolling down
@@ -55,6 +55,30 @@ document.addEventListener('scroll', () => {
 
 arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
+});
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  // console.log(filter);
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
 });
 
 function scrollIntoView(selector) {
